88163ef81e8d8fe4ca12eb34c61296bd3f13b3bb
Farid <farid.10124288@mahasiswa.unikom.ac.id>
2025-11-29 16:37:39 +0700

    updated

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,18 +1,31 @@
-import { RouterProvider } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
-import { AuthProvider } from './providers/AuthProvider'
-import { AppQueryProvider } from './providers/QueryProvider'
-import { ThemeProvider } from './providers/ThemeProvider'
-import { router } from './routes'
+import { ShellLayout } from './layout/ShellLayout'
+import { TunnelLayout } from './layout/TunnelLayout'
+import { AdminPage } from '../pages/AdminPage'
+import { AuthPage } from '../pages/AuthPage'
+import { FutureDashboardPage } from '../pages/FutureDashboardPage'
+import { FutureTunnelPage } from '../pages/FutureTunnelPage'
+import { LandingPage } from '../pages/LandingPage'
+import { NotFoundPage } from '../pages/NotFoundPage'
+import { ProfilePage } from '../pages/ProfilePage'
+import { SpherePage } from '../pages/SpherePage'
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <AppQueryProvider>
-          <RouterProvider router={router} />
-        </AppQueryProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <Routes>
+      <Route element={<ShellLayout />}>
+        <Route index element={<LandingPage />} />
+        <Route path="/future/dashboard" element={<FutureDashboardPage />} />
+        <Route path="/sphere" element={<SpherePage />} />
+        <Route path="/me" element={<ProfilePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+      <Route path="/future/tunnel" element={<TunnelLayout />}>
+        <Route index element={<FutureTunnelPage />} />
+      </Route>
+    </Routes>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
-import { AppRouter } from './app/router'
+import App from './app/App'
 import { AppProviders } from './app/providers'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppProviders>
-      <AppRouter />
-    </AppProviders>
+    <BrowserRouter>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </BrowserRouter>
   </StrictMode>,
 )
